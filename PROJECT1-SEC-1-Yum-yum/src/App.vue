6d2a8c5f83e9f00072578c8fddccbfad3e68973f
PakapolTaubol <pakapol.taubol@mail.kmutt.ac.th>
2023-02-05 14:50:15 +0700

    เพิ่ม bg

--- PROJECT1-SEC-1-Yum-yum/src/App.vue
+++ PROJECT1-SEC-1-Yum-yum/src/App.vue
@@ -43,35 +43,35 @@ function shuffleDeck() {
 </script>
 
 <template>
-  <div class="w-full">
-    <!-- Dealer -->
-    <div class="w-full py-6">
-      <div class="pt-4 flex justify-center space-x-8">
-        <img class="w-32" src='./assets/img/All-card-final/backcard/back_card.svg' ref="hiddenCard">
-        <img class="w-32" src='./assets/img/All-card-final/7-C.png'>
+    <div class="w-full py-12 bg-green-800">
+      <!-- Dealer -->
+      <div class="w-full">
+        <div class="flex justify-center space-x-8">
+          <img class="w-36" src='./assets/img/All-card-final/backcard/back_card.svg' ref="hiddenCard">
+          <img class="w-36" src='./assets/img/All-card-final/7-C.png'>
+        </div>
+        <h1 class="pt-4 flex justify-center text-white font-bold text-xl">Dealer: {{ dealerPoint }}</h1>
       </div>
-      <h1 class="pt-4 flex justify-center font-bold text-xl">Dealer: {{ dealerPoint }}</h1>
-    </div>
 
-    <!-- Button -->
-    <div class="flex justify-center w-full py-3 space-x-8">
-      <button class="px-6 items-center bg-green-500 hover:bg-green-600 active:bg-green-800 text-white 
+      <!-- Button -->
+      <div class="flex justify-center w-full py-3 space-x-8">
+        <button class="px-6 items-center bg-green-500 hover:bg-green-600 active:bg-green-800 text-white 
                font-bold text-lg text-center rounded-lg py-2"> HIT
-      </button>
-      <button class="px-6 items-center bg-red-500 hover:bg-red-600 active:bg-red-800 text-white 
+        </button>
+        <button class="px-6 items-center bg-red-500 hover:bg-red-600 active:bg-red-800 text-white 
                font-bold text-lg text-center rounded-lg py-2"> STAY
-      </button>
-    </div>
+        </button>
+      </div>
 
-    <!-- Player -->
-    <div class="w-full pt-3">
-      <h1 class="flex justify-center font-bold text-xl">Player: {{ playerPoint }}</h1>
-      <div class="flex justify-center space-x-5 pt-4">
-        <img class="w-32 " src="./assets/img/All-card-final/J-T.png">
-        <img class="w-32 " src="./assets/img/All-card-final/A-C.png">
+      <!-- Player -->
+      <div class="w-full">
+        <h1 class="flex justify-center text-white font-bold text-xl">Player: {{ playerPoint }}</h1>
+        <div class="flex justify-center space-x-5 pt-4">
+          <img class="w-36 " src="./assets/img/All-card-final/J-T.png">
+          <img class="w-36 " src="./assets/img/All-card-final/A-C.png">
+        </div>
       </div>
     </div>
-  </div>
 </template>
 
 <style scoped>
